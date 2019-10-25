3917f98ee47ca9485d03def0087e09ba649903fd
Aiden Sykes <a.sykes5222@student.leedsbeckett.ac.uk>
2019-10-25 18:28:07 +0000

    Database Migrations

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function home(){
+        
+    //Route::get('/', function () {
+    return view('welcome');
+  
+//});
+        
+    }
+    
+    public function about(){
+        //Route::get('/about', function () {
+    return view('about');
+//});        
+    }
+    
+    public function contact(){
+    //Route::get('/contact', function () {
+    return view('contact');
+//});
+    
+        
+    }
+    
+    public function grade(){
+  
+
+        return view('grade');
+      
+        
+    }
+    
+    public function project(){
+     //Route::get('/works', function () {
+    return view('project');
+//});   
+        
+    }
+    
+    public function todo(){
+        //Route::get('/todo', function () {
+      $tasks = [
+                'Sending data to views - Done!',
+                'Controllers - Done!',
+                'Database integration through migrations and the Eloquent ORM',
+                'Forms including CSRF protection and data modification',
+                'Data cleansing and validation',
+                'Registration and authentication',
+                'Authorisation and Laravel Gate',
+                'Move onto Checkpoint 2!'
+            ];
+
+return view('todo', [
+'tasks' => $tasks,
+'foo' => request('title')
+]);
+//});
+        
+    }
+    
+}
+?>
--- database/migrations/2019_10_25_182305_create_grade_table.php
+++ database/migrations/2019_10_25_182305_create_grade_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGradeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('grade', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('user');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('topic');
+            $table->string('qualification');
+            $table->string('grade');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('grade');
     }
 }
